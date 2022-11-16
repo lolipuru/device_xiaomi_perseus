4c82512fd77605f39cfae5b1aeea8018d8fcca9b
lolipuru <banstellazz@gmail.com>
2022-11-16 04:33:31 -0500

    perseus: PE bringup
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- aosp_perseus.mk
+++ aosp_perseus.mk
@@ -7,10 +7,17 @@
 $(call inherit-product, device/xiaomi/perseus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_perseus
+PRODUCT_NAME := aosp_perseus
 PRODUCT_DEVICE := perseus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
@@ -18,10 +25,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := perseus
 
-BUILD_FINGERPRINT := "Xiaomi/perseus/perseus:10/QKQ1.190828.002/V11.0.3.0.QEEMIXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="perseus-user 10 QKQ1.190828.002 V11.0.3.0.QEEMIXM release-keys" \
     TARGET_PRODUCT="perseus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
